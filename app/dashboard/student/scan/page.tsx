0b232e4e71808a347552dd27a4a6a9703ec0940f
Vishwesh-Bhilare <bhilarevishwesh@gmail.com>
2026-02-11 20:31:46 +0530

    fixed

--- app/dashboard/student/scan/page.tsx
+++ app/dashboard/student/scan/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 import { useEffect, useRef, useState } from "react";
 import { Html5Qrcode } from "html5-qrcode";
 import { useRouter, useSearchParams } from "next/navigation";
